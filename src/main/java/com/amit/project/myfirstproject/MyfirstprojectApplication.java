1280b0c815190ae9176ed8e86eb447a42f658684
Nabeel Abdullah <nabeel.abdullah@freshmenu.com>
2018-06-17 11:41:20 +0530

    commit my first project

---
+++ src/main/java/com/amit/project/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,16 @@
+package com.amit.project.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstprojectApplication {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hell3");
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+}
